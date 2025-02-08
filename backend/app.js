f5d93e7794240c8d05ee9618f45c8f6dd0f0eed8
Ankit singh <devankit2024@gmail.com>
2025-02-08 20:00:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use("/".function(req, res){
+    res.send("successfully");
+})
 
 app.use("/api/v1/message", messageRouter)
 
